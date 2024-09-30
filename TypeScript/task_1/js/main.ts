5e64bdf55ce768f309c26ecaea770eff02bcb080
Christian lopez <christianlopezrivera0@gmail.com>
2024-09-30 02:01:15 -0400

    Extending the Teacher class

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -9,6 +9,11 @@ interface Teacher {
     [key: string]: any; // allows any additional properties
   }
   
+  // Directors interface extending Teacher
+  interface Directors extends Teacher {
+    numberOfReports: number; // required property
+  }
+  
   const teacher3: Teacher = {
     firstName: 'John',
     lastName: 'Doe',
@@ -18,4 +23,14 @@ interface Teacher {
   };
   
   console.log(teacher3);
+  
+  const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  
+  console.log(director1);
   
